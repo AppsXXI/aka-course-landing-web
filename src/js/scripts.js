a2d68825795e27f7ca3a2cc0c9ddf34c7c2acaff
Miguel Angel Sosa Menchaca <miguel.sosa@globant.com>
2019-02-15 22:06:55 -0300

    feature/navigation-on-scroll - Added behavior to nav on scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     element.removeAttribute('disabled');
   });
 });
+
+document.addEventListener('scroll', function (event) {
+  var body = document.body;
+  var html = document.documentElement;
+  var scrollTop = body.scrollTop || html.scrollTop;
+  var windowHeight = window.outerHeight;
+  var firstNav = document.querySelector('.first-nav');
+
+  // console.log(windowHeight)
+  if (scrollTop >= windowHeight) {
+    firstNav.classList.add('scrolling');
+  } else {
+    firstNav.classList.remove('scrolling');
+  }
+});
