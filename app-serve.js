fb98a9322c93fb351f5f2f8e645883c10d815328
Miguel Angel Sosa Menchaca <miguel.sosa@globant.com>
2019-02-15 23:02:13 -0300

    feature/be-endpoints-forms - Added first endpoint to test sendgrid

--- app-serve.js
+++ app-serve.js
@@ -11,6 +11,27 @@ app.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+app.post('/process-more-info-form', function (req, res) {
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  console.log(process.env.SENDGRID_API_KEY);
+  console.log(req.body);
+  
+  const adminMsg = {
+    to: 'miguel.sosa@appsxxi.com',
+    from: req.body.fullname + ' <' + req.body.email + '>',
+    subject: '[CDPW - More Info] - ' + req.body.fullname,
+    text: 'Requesting more info',
+    html: '<strong>Requesting more info</strong>',
+  };
+
+  const userMsg = {};
+
+  sgMail.send(msg);
+  res.status(200).send({ status: 'ok' });
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server has started");
 });
